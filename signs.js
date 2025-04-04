72cdf93a66d0de5c4bae691e08a36c35020aa49a
ticuv <142942766+ticuv@users.noreply.github.com>
2025-04-04 11:22:28 +0300

    Create signs.js

---
+++ signs.js
@@ -0,0 +1,32 @@
+{
+  "githubUser": "ticuv",
+  "githubRepo": "SignOMatic",
+  "githubBranch": "main",
+  "imageBasePath": "images",
+  "categories": {
+    "AHC": [
+      {
+        "name": "AHC - Ape",
+        "fileName": "ape.png"
+      },
+      {
+        "name": "AHC - Delist",
+        "fileName": "delist.png"
+      }
+    ],
+    "GHN": [
+      {
+        "name": "GHN - Daniel",
+        "fileName": "daniel.png"
+      },
+      {
+        "name": "GHN - Delist",
+        "fileName": "delist.png"
+      },
+      {
+        "name": "GHN - Duko",
+        "fileName": "duko.png"
+      }
+    ]
+  }
+}
